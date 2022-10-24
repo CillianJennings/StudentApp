4c4fcf6bd741ebd1718a6e066142fb044844517b
Cillian <G00404411@atu.ie>
2022-10-24 10:03:24 +0100

    Fixed prompt code and added getters and setters in the Student class

--- src/main/java/ie/atu/StudentApp/StudentApp.java
+++ src/main/java/ie/atu/StudentApp/StudentApp.java
@@ -4,8 +4,15 @@ import java.util.Scanner;
 
 public class StudentApp {
     public static void main(String[] args) {
+
         System.out.println("Please enter your name:");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+
         System.out.println("Please enter your student email:");
+        String email = input.nextLine();
+
         System.out.println("Please enter the name of your course:");
+        String course = input.nextLine();
     }
 }
