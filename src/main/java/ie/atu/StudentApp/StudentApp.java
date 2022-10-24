5ade761ec54361b5ce799c879288ab3bee3df0fa
Cillian <G00404411@atu.ie>
2022-10-24 10:36:20 +0100

    Added code that makes use of the getters and setters in the student class

--- src/main/java/ie/atu/StudentApp/StudentApp.java
+++ src/main/java/ie/atu/StudentApp/StudentApp.java
@@ -7,12 +7,19 @@ public class StudentApp {
 
         System.out.println("Please enter your name:");
         Scanner input = new Scanner(System.in);
+        Student studentInfo = new Student();
         String name = input.nextLine();
+        studentInfo.setName(name);
+        System.out.println("You entered: " + studentInfo.getName());
 
         System.out.println("Please enter your student email:");
         String email = input.nextLine();
+        studentInfo.setEmail(email);
+        System.out.println("You entered: " + studentInfo.getEmail());
 
         System.out.println("Please enter the name of your course:");
         String course = input.nextLine();
+        studentInfo.setCourse(course);
+        System.out.println("You entered: " + studentInfo.getCourse());
     }
 }
