48ff2faed7e7e8916b2a31f0f7cfbb124e3d8088
Cillian <G00404411@atu.ie>
2022-10-27 16:28:21 +0100

    Added some comments

--- src/main/java/ie/atu/StudentApp/Student.java
+++ src/main/java/ie/atu/StudentApp/Student.java
@@ -10,12 +10,15 @@ public class Student {
         this.email = "";
         this.course = "";
     }
+
+    //Constructor
     public Student(String name, String email, String course) {
         this.name = name;
         this.email = email;
         this.course = course;
     }
 
+    //getters and setters
     public String getName() {
         return name;
     }
